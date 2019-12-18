7ddcde28d0e5136283c7461cef15a545d3cbbffd
Naveen Kashyap <naveenkashyap243@gmail.com>
2019-12-18 23:11:41 +0530

    add template string in firebase config

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
   apiKey: process.env.VUE_APP_APIKEY,
   authDomain: process.env.VUE_APP_AUTHDOMAIN,
   databaseURL: process.env.VUE_APP_DATABASEURL,
-  projectId: process.env.VUE_APP_PROJECTID,
+  projectId: `${process.env.VUE_APP_PROJECTID}`,
   storageBucket: process.env.VUE_APP_STORAGEBUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
   appId: process.env.VUE_APP_APPID
